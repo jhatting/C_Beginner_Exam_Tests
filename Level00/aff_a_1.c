2d8acba223358e52da1d4b89770c8c6a3762db57
Jarrod Hatting <jarrodhatting@icloud.com>
2020-02-27 14:42:33 -0800

    update

---
+++ Level00/aff_a_1.c
@@ -0,0 +1,38 @@
+/* -.-.-.-.-.-.-.-.-.-.-.-.-.-.-.-.-.-.-.-.
+
+* File Name : aff_a_1.c
+
+* Purpose :Takes a string and displays the first :"a": it encounters followed \n . If there is no "a" characters in the strings , writes only \n.
+If the number of parameters is not 1, the program displays "a\n" 
+
+* Creation Date : 27-02-2020
+
+* Last Modified : Thu Feb 27 14:42:30 2020
+
+* Created By : Jarrod Hatting
+_._._._._._._._._._._._._._._._._._._._._.*/
+
+#include <unistd.h>
+
+int main(int argc, char *argv[])
+{
+    int i = 0;
+    
+    if(argc != 2)
+        write(1, "a\n", 1);
+        else
+        {
+            while (argv[1][i])
+            {
+                if (argv[1][i] != 'a')
+                    i++;
+                    else
+                    {
+                        write(1, "a", 1);
+                        break;
+                    }
+            }
+            write(1, "\n", 1);
+        }
+        return (0);
+}
