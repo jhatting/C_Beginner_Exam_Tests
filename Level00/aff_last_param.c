1d71b1365aec1ec2ddac1dc55f4b2ee50687f094
Jarrod Hatting <jarrodhatting@icloud.com>
2020-02-27 10:18:34 -0800

    update

--- Level00/aff_last_param.c
+++ Level00/aff_last_param.c
@@ -6,9 +6,22 @@
 
 * Creation Date : 25-02-2020
 
-* Last Modified : Thu Feb 27 09:25:00 2020
+* Last Modified : Thu Feb 27 10:17:39 2020
 
 * Created By : Jarrod Hatting
 _._._._._._._._._._._._._._._._._._._._._.*/
 
-#include <un>
+#include <unistd.h>
+
+int main (int argc, char *argv[])
+{
+    int i = 0;
+
+    if (argc >= 1)
+        {
+            while (argv[argc -1][i])
+                write(1, &argv[argc -1][i++], 1);
+        }
+        write(1,"\n",1);
+        return(0);
+}
