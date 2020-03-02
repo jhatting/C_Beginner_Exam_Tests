8518dde9154fda7f275a86cbbed98e2734b3720d
Jarrod Hatting <jarrodhatting@icloud.com>
2020-03-02 13:10:53 -0800

    update

---
+++ Level00/maff_revalpha.c
@@ -0,0 +1,20 @@
+/* -.-.-.-.-.-.-.-.-.-.-.-.-.-.-.-.-.-.-.-.
+
+* File Name : maff_revalpha.c
+
+* Purpose : write a program that displays the alphabet in reverse , with even letters in uppercase, and odd letters in lowercase followed by a newline.
+
+* Creation Date : 02-03-2020
+
+* Last Modified : Mon Mar  2 13:10:05 2020
+
+* Created By : Jarrod Hatting
+_._._._._._._._._._._._._._._._._._._._._.*/
+
+#include <unistd.h>
+
+int main(void)
+{
+    write(1, "zYxWvUtSrQpOnMlKjIhGfEdCbA\n", 27);
+    return(0);
+}
