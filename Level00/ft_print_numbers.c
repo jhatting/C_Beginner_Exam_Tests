46bc341648cf5abbb831624813af612516280de4
Jarrod Hatting <jarrodhatting@icloud.com>
2020-03-02 13:27:56 -0800

    update

---
+++ Level00/ft_print_numbers.c
@@ -0,0 +1,20 @@
+/* -.-.-.-.-.-.-.-.-.-.-.-.-.-.-.-.-.-.-.-.
+
+* File Name : ft_print_numbers.c
+
+* Purpose : a function that displays all digits in ascending order. 
+
+your function must be declared as follows: 
+void ft_print_numbers(void)
+
+* Creation Date : 02-03-2020
+
+* Last Modified :
+
+* Created By : Jarrod Hatting
+_._._._._._._._._._._._._._._._._._._._._.*/
+
+void ft_print_numbers(void)
+{
+    write(1, "1234567890\n", 11);
+}
