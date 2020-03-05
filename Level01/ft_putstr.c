8be8ac39d172779750aa8893e6e73bb90ec85370
Jarrod Hatting <jarrodhatting@icloud.com>
2020-03-04 17:13:06 -0800

    update

---
+++ Level01/ft_putstr.c
@@ -0,0 +1,18 @@
+/* -.-.-.-.-.-.-.-.-.-.-.-.-.-.-.-.-.-.-.-.
+
+* File Name : ft_putstr.c
+
+* Purpose : displays a string on the standard output / 
+The pointer passed to the function contains the address of the strings first character.
+
+* Creation Date : 04-03-2020
+
+* Last Modified : Wed Mar  4 17:12:55 2020
+
+* Created By : Jarrod Hatting
+_._._._._._._._._._._._._._._._._._._._._.*/
+
+
+void ft_putstr(char *str);
+
+
