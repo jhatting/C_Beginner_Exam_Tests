baa12d4100d14406ad0e5921f17ee40478967b36
Jarrod Hatting <jarrodhatting@icloud.com>
2020-03-04 17:16:27 -0800

    update

--- Level01/ft_putstr.c
+++ Level01/ft_putstr.c
@@ -7,12 +7,26 @@ The pointer passed to the function contains the address of the strings first cha
 
 * Creation Date : 04-03-2020
 
-* Last Modified : Wed Mar  4 17:12:55 2020
+* Last Modified : Wed Mar  4 17:16:22 2020
 
 * Created By : Jarrod Hatting
 _._._._._._._._._._._._._._._._._._._._._.*/
 
+#include <unistd.h>
 
 void ft_putstr(char *str);
 
+int main(void)
+{
+    ft_putstr(char *str)
+    return(0);
+}
+
+int ft_strlen(char *str)
+{
+    int i = 0;
+    while (str[i] != '\0')
+        ++i;
+        return (i);
+}
 
