24c3d6db33459b4e39cb3722f24048c883b8fdad
Jarrod Hatting <jarrodhatting@icloud.com>
2020-03-06 09:18:33 -0800

    update

---
+++ Level01/ft_swap.c
@@ -0,0 +1,37 @@
+/* -.-.-.-.-.-.-.-.-.-.-.-.-.-.-.-.-.-.-.-.
+
+* File Name : ft_swap.c
+
+* Purpose : function that swaps the contents of two intergers the addresses of which are passed as parameters.
+
+function must be declared as follows:
+
+void ft_swap(int *a, int *b);
+
+* Creation Date : 06-03-2020
+
+* Last Modified :
+
+* Created By : Jarrod Hatting
+_._._._._._._._._._._._._._._._._._._._._.*/
+
+#include <unistd.h>
+#include <stdio.h>
+
+//void ft_swap(char *a, char *b)
+//{
+//    int swap = *a;
+//    *a = *b;
+//    *b = swap;
+//}
+
+int main()
+{
+    char str1 [] = "Jarrod";
+    char str2 [] = "sam";
+    
+    printf("str1 = %s, str2 = %s", str1, str2);
+
+}
+
+
