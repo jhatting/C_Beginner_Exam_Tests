edfe2281e2d7aff74ac3ba511d1487adffc4dfbc
Jarrod Hatting <jarrodhatting@icloud.com>
2020-03-09 10:58:56 -0700

    update

---
+++ Level00/aff_a_7.c
@@ -0,0 +1,13 @@
+/* -.-.-.-.-.-.-.-.-.-.-.-.-.-.-.-.-.-.-.-.
+
+* File Name : aff_a_7.c
+
+* Purpose : takes a string , and displays  the first 'a' character it encounters in it ,followed by a newline. If there are no 'a' characters in the string the program just writes a newline. If the number of parameters is not 1, the program 'a' followed by a newline.
+
+
+* Creation Date : 09-03-2020
+
+* Last Modified : Mon Mar  9 10:58:52 2020
+
+* Created By : Jarrod Hatting
+_._._._._._._._._._._._._._._._._._._._._.*/
