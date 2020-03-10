fc943975edb077a4b2c534b04c3a931fcbe20358
Jarrod Hatting <jarrodhatting@icloud.com>
2020-03-10 08:38:07 -0700

    update

--- Level00/aff_last_param_4.c
+++ Level00/aff_last_param_4.c
@@ -6,28 +6,25 @@
 
 * Creation Date : 10-03-2020
 
-* Last Modified : Tue Mar 10 08:28:16 2020
+* Last Modified : Tue Mar 10 08:36:52 2020
 
 * Created By : Jarrod Hatting
 _._._._._._._._._._._._._._._._._._._._._.*/
 
 #include <unistd.h>
 
-void    ft_putstr(char *str)
+void ft_putstr(char *str)
 {
-    int i;
-
-    i = 0;
-    while (str[i])
-        write(1, &str[i++], 1);
+    int i = 0;
+    while (str[i] != '\0')
+        i++;
+        write(1, str, i);
 }
 
 int main (int argc, char *argv[])
 {
-    int i = 0;
-
-    if (argc == 2)
-        ft_putstr(argv[argc-1]);
-    write(1, "\n",1);
-    return (0);
+    if (argc > 1)
+        ft_putstr(argv[argc - 1]);
+        write(1, "\n", 1);
+        return(0);
 }
