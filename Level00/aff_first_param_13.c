d2007d81e2e9af64cc3951733f6397fa2cd78ba1
Jarrod Hatting <jarrodhatting@icloud.com>
2020-03-24 17:01:43 -0700

    update

--- Level00/aff_first_param_13.c
+++ Level00/aff_first_param_13.c
@@ -34,16 +34,15 @@ int ft_strlen(char *str)
 {
     int i = 1;
 
-    if (str[i] != '\0')
+    while (str[i] != '\0')
         i++;
         return(i);
 }
 
 int main(int argc, char *argv[])
 {
-    int i = 0;
-    if (argc >=2 )
-        write(1, argv[1], ft_strlen(argv[i]));
+    if (argc != 1 )
+        write(1, argv[1], ft_strlen(argv[1]));
         write(1, "\n", 1);
         return(0);
 }
