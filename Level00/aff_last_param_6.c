34536e86785293607a241221326f42d9f2a33b34
Jarrod Hatting <jarrodhatting@icloud.com>
2020-03-29 09:26:11 -0700

    update

--- Level00/aff_last_param_6.c
+++ Level00/aff_last_param_6.c
@@ -6,7 +6,7 @@
 
 * Creation Date : 29-03-2020
 
-* Last Modified : Sun Mar 29 09:24:49 2020
+* Last Modified : Sun Mar 29 09:26:01 2020
 
 * Created By : Jarrod Hatting
 _._._._._._._._._._._._._._._._._._._._._.*/
@@ -24,7 +24,7 @@ int ft_strlen(char *str)
 int main(int argc, char *argv[])
 {
     if (argc >= 2)
-        write(1, argv[1], ft_strlen(argv[1]));
+        write(1, argv[argc - 1], ft_strlen(argv[argc - 1]));
         write(1, "\n", 1);
         return(0);
 }
