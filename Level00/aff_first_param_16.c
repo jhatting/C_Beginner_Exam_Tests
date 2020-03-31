99984d522c893f8d64a4043c7f7ea6a7709c1503
Jarrod Hatting <jarrodhatting@icloud.com>
2020-03-31 12:04:09 -0700

    update

--- Level00/aff_first_param_16.c
+++ Level00/aff_first_param_16.c
@@ -43,7 +43,7 @@ int ft_strlen(char *str)
 int main(int argc, char *argv[])
 {
     if (argc >= 2)
-        write(1, argv[1], ft_strlen(argv[1]);
+        write(1, argv[1], ft_strlen(argv[1]));
         else 
             write(1, "\n", 1);
             return(0);
