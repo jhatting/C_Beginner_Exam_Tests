4785d4182a9615b0a2a3a2f6aba16a7bd29abf83
jhatting <jarrodhatting@icloud.com>
2020-05-04 17:42:17 -0700

    update

--- Level00/aff_first_param_38.c
+++ Level00/aff_first_param_38.c
@@ -28,3 +28,15 @@ $
 
 * Created By : Jarrod Donovan Hatting 
 _._._._._._._._._._._._._._._._._._._._._.*/
+
+#include <unistd.h>
+
+int ft_strlen(char *str)
+{
+    int i =0 ;
+
+    while (str[i] != '\0')
+        i++;
+        return(i);
+}
+
