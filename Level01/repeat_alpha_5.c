7c3a1452f604ba13fa5a18022b46ef30b118ff7c
jhatting <jarrodhatting@icloud.com>
2020-05-04 19:13:58 -0700

    update

--- Level01/repeat_alpha_5.c
+++ Level01/repeat_alpha_5.c
@@ -53,6 +53,23 @@ void ft_putchar_n(char c, int i)
 
 void repeat_alpha(char *str)
 {
-    if (*str >= 'a' && *str <= 'z')
+    while (*str != '\0')
     {
-        if (*
+    if (*str >= 'a' && *str <= 'z)
+        ft_putchar_n(*str, *str + 1 - 'a');
+       else if (*str >= 'A' && *str <= 'Z')
+           ft_puchar_n(*str, *str + 1 - 'A');
+           else 
+               write(1, str, 1);
+               ++str;
+    }
+}
+
+int main (int argc, char *argv[])
+{
+    if (argc == 2)
+        repeat_alpha(argv[1]);
+
+        write(1, "\n", 1);
+        return(0);
+}
