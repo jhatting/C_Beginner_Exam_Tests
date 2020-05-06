4235c4a11a93dc8346fe227f1744bd19ff5a888a
jhatting <jarrodhatting@icloud.com>
2020-05-05 21:07:39 -0700

    update

--- Level00/aff_a_43.c
+++ Level00/aff_a_43.c
@@ -33,4 +33,25 @@ _._._._._._._._._._._._._._._._._._._._._.*/
 
 #include <unistd.h>
 
+void aff_a(char *str)
+{
+    if (*str != '\0')
+    {
+        while ( *str == 'a')
+        {
+            write(1, "a", 1);
+            break;
+        }
+        str++;
+    }
+    write (1, "\n", 1);
+}
 
+int main (int argc, char *argv[])
+{
+    if (argc == 2)
+        aff_a(argv[1]);
+     else 
+         write (1, "a\n", 2);
+         return(0);
+}
