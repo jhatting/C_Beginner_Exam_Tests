9ea4dc7f9f28b82d1bbbe9236d42106644bb0131
jhatting <jarrodhatting@icloud.com>
2020-05-09 20:53:47 -0700

    update

--- Level01/rev_print_7.c
+++ Level01/rev_print_7.c
@@ -37,6 +37,7 @@ void rev_print(char *str)
 
     while (str[i] != '\0')
         ++i;
+      --i;
         while (i >= 0)
        {
            write(1, str+ i, 1);
@@ -48,5 +49,6 @@ int main(int argc, char *argv[])
 {
     if (argc == 2)
         rev_print(argv[1]);
+
         return(0);
 }
