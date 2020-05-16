e5989661e913ad6093536996c90569bd73f5d966
Jarrod Hatting <jarrodhatting@icloud.com>
2020-05-16 15:09:59 -0700

    update

--- Level00/aff_a_46.c
+++ Level00/aff_a_46.c
@@ -50,7 +50,7 @@ void aff_a(char *str)
 int main (int argc, char *argv[])
 {
     if (argc == 2)
-        aff_a(argv[]);
+        aff_a(argv[1]);
         else 
             write(1, "a\n", 2);
             return(0);
