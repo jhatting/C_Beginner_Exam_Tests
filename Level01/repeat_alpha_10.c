d3e7913e2718cc30c3d67ef4d2dcccca84cfe655
Jarrod Hatting <jarrodhatting@icloud.com>
2020-05-23 09:12:16 -0700

    update

--- Level01/repeat_alpha_10.c
+++ Level01/repeat_alpha_10.c
@@ -38,3 +38,32 @@ $>:
 
 * Created By : Jarrod Hatting
 _._._._._._._._._._._._._._._._._._._._._.*/
+
+#include <unistd.h>
+
+void (ft_putchar_n(char c, int i)
+{
+    while (i > 0)
+    {
+        write(1, &c, 1);
+        --i;
+    }
+}
+
+void repeat_alpha(char *str)
+{
+    while (*str != '\0')
+    {
+        if (*str >= 'a' && *str <= 'z')
+            ft_putchar_n(*str, *str + 1 - 'a');
+            else if (*str >= 'A' && *str <= 'Z')
+                ft_putchar_(*str, *str + 1 - 'A');
+                else
+                    write(1, str, 1);
+                    ++str;
+    }
+}
+
+int main (int argc, char *argv[])
+{
+    if (
