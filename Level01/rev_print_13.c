7c9a60e9ea02551fcb8e31d506dc6bb230529e8f
Jarrod Hatting <jarrodhatting@icloud.com>
2020-05-24 10:15:56 -0700

    update

--- Level01/rev_print_13.c
+++ Level01/rev_print_13.c
@@ -4,6 +4,25 @@
 
 * Purpose :
 
+Assignment name  : rev_print
+Expected files   : rev_print.c
+Allowed functions: write
+--------------------------------------------------------------------------------
+
+Write a program that takes a string, and displays the string in reverse
+followed by a newline.
+
+If the number of parameters is not 1, the program displays a newline.
+
+Examples:
+
+$> ./rev_print "rainbow dash" | cat -e
+hsad wobniar$
+$> ./rev_print "Ponies are awesome" | cat -e
+emosewa era seinoP$
+$> ./rev_print | cat -e
+$
+
 * Creation Date : 24-05-2020
 
 * Last Modified : Sun May 24 10:15:15 2020
