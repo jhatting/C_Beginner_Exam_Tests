181542ff600cf7a9fadaa0dc257a1568904ad1c6
Jarrod Hatting <jarrodhatting@icloud.com>
2020-05-24 10:18:18 -0700

    update

--- Level01/rev_print_13.c
+++ Level01/rev_print_13.c
@@ -29,3 +29,20 @@ $
 
 * Created By : Jarrod Hatting
 _._._._._._._._._._._._._._._._._._._._._.*/
+
+#include <unistd.h>
+
+void rev_print(char *str)
+{
+    int i = 0;
+
+    while (str[i] != '\0')
+        ++i;
+     --i;
+     while (i >= 0)
+     {
+         write(1, str + i, 1);
+         --i;
+     }
+}
+
