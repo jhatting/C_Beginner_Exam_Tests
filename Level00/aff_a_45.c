80d02ea6c562a1bd532fb3d57244af8fb5a54b13
jhatting <jarrodhatting@icloud.com>
2020-05-26 11:52:45 -0700

    update

--- Level00/aff_a_45.c
+++ Level00/aff_a_45.c
@@ -40,7 +40,7 @@ _._._._._._._._._._._._._._._._._._._._._.*/
 
 void aff_a(char *str)
 {
-    while (str* != '\0')
+    while (*str != '\0')
     {
         while (*str == 'a')
         {
@@ -50,19 +50,6 @@ void aff_a(char *str)
         str++;
     }
     write(1, "\n", 1);
-}
-
-int main(it argc, char *argv[])
-{
-    if (argc == 2)
-        aff_a(argv[1]);
-      else
-          write(1, "a\n", 2);
-          return(0);
-}
-
-
-    write (1, "\n", 1);
 }
 
 int main (int argc, char *argv[])
