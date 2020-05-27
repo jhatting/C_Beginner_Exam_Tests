7496680e0510e01dc49a678d700ae13f2677ba15
jhatting <jarrodhatting@icloud.com>
2020-05-26 17:17:03 -0700

    update

---
+++ Level01/rev_print_18.c
@@ -0,0 +1,55 @@
+/* -.-.-.-.-.-.-.-.-.-.-.-.-.-.-.-.-.-.-.-.
+
+* File Name : rev_print_18.c
+
+* Purpose :
+
+Assignment name  : rev_print
+Expected files   : rev_print.c
+Allowed functions: write
+--------------------------------------------------------------------------------
+
+Write a program that takes a string, and displays the string in reverse
+followed by a newline.
+
+If the number of parameters is not 1, the program displays a newline.
+
+Examples:
+
+$> ./rev_print "rainbow dash" | cat -e
+hsad wobniar$
+$> ./rev_print "Ponies are awesome" | cat -e
+emosewa era seinoP$
+$> ./rev_print | cat -e
+$
+
+* Creation Date : 26-05-2020
+
+* Last Modified :
+
+* Created By : Jarrod Donovan Hatting 
+_._._._._._._._._._._._._._._._._._._._._.*/
+
+#include <unistd.h>
+
+void rev_print(char *str)
+{
+    int i = 0;
+
+    while (i > 0)
+        ++i;
+       --i;
+       while (*str != '\0')
+       {
+           write(1, str + i, 1);
+           --i;
+       }
+}
+
+int main(int argc, char *argv[])
+{
+    if (argc == 2)
+        rev_print(argv[1]);
+        write(1, "\n", 1);
+        return(0);
+}
