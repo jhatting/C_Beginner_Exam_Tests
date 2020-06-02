86fb1898784f3d93735a3204292d466b1574f3bc
Jarrod Hatting <jarrodhatting@icloud.com>
2020-06-01 19:42:13 -0700

    update

--- Level00/first_param_44.c
+++ Level00/first_param_44.c
@@ -41,7 +41,7 @@ int ft_strlen(char *str)
 int main (int argc, char *argv[])
 {
     if (argc >= 2)
-        write(1, argv[1], ft_strlen(argv[1]);
+        write(1, argv[1], ft_strlen(argv[1]));
        write(1, "\n", 1);
        return(0);
 }
