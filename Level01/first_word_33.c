0ac0180bb116758fa27336716649f66e93044ef8
Jarrod Hatting <jarrodhatting@icloud.com>
2020-06-05 14:47:35 -0700

    update

--- Level01/first_word_33.c
+++ Level01/first_word_33.c
@@ -44,7 +44,7 @@ void first_word(char *str )
 {
    while (*str == ' ' || *str == '\t')
        str++;
-       while (*str != ' ' && *str != '\t' && *str == '\0')
+       while (*str != ' ' && *str != '\t' && *str != '\0')
        {
            write(1, str, 1);
            str++;
@@ -54,7 +54,7 @@ void first_word(char *str )
 int main (int argc, char *argv[])
 {
     if (argc == 2)
-        first_word(argv[1])
+        first_word(argv[1]);
         write(1, "\n", 1);
         return(0);
 }
